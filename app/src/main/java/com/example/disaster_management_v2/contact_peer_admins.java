6923633cac9e964d5eff440073263d5f7c2f4ccc
Naitik Rathod <rathodnaitik10@gmail.com>
2019-09-01 03:29:23 +0530

    contact_peer_admins file updated

--- app/src/main/java/com/example/disaster_management_v2/contact_peer_admins.java
+++ app/src/main/java/com/example/disaster_management_v2/contact_peer_admins.java
@@ -3,15 +3,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
